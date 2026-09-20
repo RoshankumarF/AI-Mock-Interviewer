a058868fe65fd762e2e48154e5cca6d6fc629b47
Roshan Kumar <veerroshankumar@gmail.com>
2026-09-20 16:45:21 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+ 
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='items-center-safe text-3xl text-red-600'> Hello there welcome to interview prep</h1>
+    </>
+  )
+}
+
+export default App
